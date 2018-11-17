ee13fe0bb3de651408cbe14cde479369a06b69b7
hbrooker97 <42081142+hbrooker97@users.noreply.github.com>
2018-11-18 01:00:41 +0800

    added 3rd show

--- js/data.js
+++ js/data.js
@@ -5,6 +5,11 @@ var data = [
     "tickets": "https://www.ptt.wa.gov.au/venues/his-majestys-theatre/whats-on/moonshinin/"
   },
   {
+    "image": "images/shows/variety.jpg",
+    "description": "What happens when you put Perth's very best jazz musicians, singers, dancers, burlesque performers and killer androgyny queens into one show? You get A Very Good Variety Show. If satisfaction is your thing, then let us fill your cup. Full of many very good parts, bits and things, it's very good! <br><br>We think a variety show should be very good, so our very good show features four-time FRINGE WORLD award-winning Perth Cabaret Collective, Fringe super-stars, Jessie Gordon and Mark Turner, several very good burlesque performers including The Sugar Duchess and one very, very good androgyny queen, Cougar Morrison. They are all very good.<br><br>\"I thought it was very good.\" - punter ",
+    "tickets": "https://fringeworld.com.au/whats_on/a-very-good-variety-show-fw2019"
+  },
+  {
     "image": "images/shows/glamourpuss.jpg",
     "description": "Glamourpuss, 11-16 Feb 2019",
     "tickets": "https://fringeworld.com.au/whats_on/glamourpuss-fw2019"
