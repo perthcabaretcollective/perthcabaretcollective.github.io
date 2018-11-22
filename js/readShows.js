ec4d8438e8d17da9a1d1149a19bc49efd4a90e6b
hbrooker97 <42081142+hbrooker97@users.noreply.github.com>
2018-11-23 00:34:33 +0800

    dated

--- js/readShows.js
+++ js/readShows.js
@@ -23,16 +23,49 @@ for (i = 0; i < dataShows.length; i++) {
     var showImage = dataShows[i].image;
     var showDesc = dataShows[i].description;
     var showTickets = dataShows[i].tickets;
-    var showStartDate = dataShows[i].startDate.toDateString();
-    var showEndDate = dataShows[i].endDate.toDateString();
+    var showStartDate = dataShows[i].startDate;
+    var showEndDate = dataShows[i].endDate;
 
-    var newShow = "<div class=\"row\">\
+
+    if (showEndDate < now) {
+        pastShows = pastShows + "<div class=\"row\">\
 <div class=\"col-md-5\">\
 <img src=\"" + showImage + "\" class=\"img-responsive\">\
 </div>\
 <div class=\"col-md-7\">\
         <h1>" + showTitle + "</h1>\
-        <h3>" + showStartDate + " - " + showEndDate + "</h3>\
+        <h3>" + showStartDate.toDateString() + " - " + showEndDate.toDateString() + "</h3>\
+        <h4><a href=\"" + showVenueLink + "\">" + showVenue + "</a></h4 >\
+        <br>\
+        <button type=\"button\" class=\"btn btn-custom\" data-toggle=\"modal\" data-target=\"#" + i + "Modal\">More info</button>\
+        <br><br>\
+        <div class=\"modal fade\" id=\"" + i + "Modal\" role=\"dialog\">\
+        <div class=\"modal-dialog\">\
+            <div class=\"modal-content\">\
+            <div class=\"modal-header\">\
+                <button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button>\
+                <h4 class=\"modal-title\">"+ showTitle + "</h4>\
+            </div>\
+          <div class=\"modal-body\">\
+                <p>" + showDesc + "</p >\
+</div>\
+    <div class=\"modal-footer\">\
+                <button type=\"button\" class=\"btn btn-custom\" data-dismiss=\"modal\">Close</button>\
+            </div>\
+    </div >\
+    </div >\
+        </div>\
+    </div>\
+</div><hr/>";
+    }
+    else {
+        shows = shows + "<div class=\"row\">\
+<div class=\"col-md-5\">\
+<img src=\"" + showImage + "\" class=\"img-responsive\">\
+</div>\
+<div class=\"col-md-7\">\
+        <h1>" + showTitle + "</h1>\
+        <h3>" + showStartDate.toDateString() + " - " + showEndDate.toDateString() + "</h3>\
         <h4><a href=\"" + showVenueLink + "\">" + showVenue + "</a></h4 >\
         <br>\
         <button type=\"button\" class=\"btn btn-custom\" data-toggle=\"modal\" data-target=\"#" + i + "Modal\">More info</button>\
@@ -43,7 +76,7 @@ for (i = 0; i < dataShows.length; i++) {
             <div class=\"modal-content\">\
             <div class=\"modal-header\">\
                 <button type=\"button\" class=\"close\" data-dismiss=\"modal\">&times;</button>\
-                <h4 class=\"modal-title\">"+ showTitle +"</h4>\
+                <h4 class=\"modal-title\">"+ showTitle + "</h4>\
             </div>\
           <div class=\"modal-body\">\
                 <p>" + showDesc + "</p >\
@@ -56,13 +89,10 @@ for (i = 0; i < dataShows.length; i++) {
     </div >\
         </div>\
     </div>\
-</div><hr/>";
-    if (showEndDate < now) {
-        pastShows = pastShows + newShow;
-        console.log("show in the past");
-    } else {
-        shows = shows + newShow;
+</div><hr/>";;
     }
+    
+    
 }
 showPage.innerHTML = shows;
 pastShowPage.innerHTML = pastShows;
