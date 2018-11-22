be5a43f80417ab446a51eb46fb9237be58b8c8ce
hbrooker97 <42081142+hbrooker97@users.noreply.github.com>
2018-11-22 14:48:45 +0800

    shows variables

--- js/readShows.js
+++ js/readShows.js
@@ -1,12 +1,24 @@
-﻿console.log(data) // {"a" : "b", "c" : "d"}
+﻿console.log(dataShows) // {"a" : "b", "c" : "d"}
 var showPage = document.getElementById("showPage");
 var shows = "<hr />";
 var i;
-for (i = 0; i < data.length; i++) {
-    var showImage = data[i].image;
-    var showDesc = data[i].description;
-    var showTickets = data[i].tickets;
-    shows = shows + "<div class=\"row\"><div class=\"col-md-5\"><img src=\"" + showImage + "\" class=\"img-responsive\"></div><div class=\"col-md-7\"><p>" + showDesc + "</p ><p><a href = \"" + showTickets + "\" target=\"_blank\" class=\"btn btn-lg btn-custom\" role=\"button\"> Tickets</a></p></div></div ><hr/>";
+for (i = 0; i < dataShows.length; i++) {
+    var showTitle = dataShows[i].title;
+    var showVenue = dataShows[i].venue;
+    var showVenueLink = dataShows[i].venueLink;
+    var showImage = dataShows[i].image;
+    var showDesc = dataShows[i].description;
+    var showTickets = dataShows[i].tickets;
+    var showDates = dataShows[i].dates;
+
+    shows = shows + "<div class=\"row\">\
+<div class=\"col-md-5\">\
+<img src=\"" + showImage + "\" class=\"img-responsive\">\
+</div>\
+<div class=\"col-md-7\">\
+<p>" + showDesc + "</p >\
+<p><a href = \"" + showTickets + "\" target=\"_blank\" class=\"btn btn-lg btn-custom\" role=\"button\"> Tickets</a></p>\
+</div></div ><hr/>";
 
 }
 showPage.innerHTML = shows;
