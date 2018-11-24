c6d82c90c5442f6b85603fc9d45669740be0d4e1
Jess <harry.brooker999@gmail.com>
2018-11-24 16:21:11 +0800

    add reviews

--- js/readReviews.js
+++ js/readReviews.js
@@ -1,11 +1,17 @@
 var reviewBox = document.getElementById("reviewBox");
 var reviews = "";
 var iii;
+var j;
 
 for(iii = 0; iii< reviewData.length; iii++){
   reviewDesc = reviewData[iii].description;
   reviewAuthor = reviewData[iii].author;
+  reviewStars = reviewData[iii].stars;
 
-  reviews = reviews + "\"" + reviewDesc + "\"" + "<br>" + "- " + reviewAuthor + "<hr>";
+  reviews = reviews + "<hr>" + "<i style=\"font-size:28px\">\"" + reviewDesc + "\"</i>" + "<br>" + "<p style=\"font-size:18px\">- " + reviewAuthor + " ";
+  for(j = 0; j < reviewStars; j++){
+    reviews = reviews + "&#9733";
+  }
+  reviews = reviews + "</p>";
 }
 reviewBox.innerHTML = reviews;
--- js/reviews.js
+++ js/reviews.js
@@ -2,12 +2,12 @@ var reviewData = [
     {
         "description": "A Very Good Show",
         "author": "Ron Swanson",
-
+        "stars": 4
     },
     {
         "description": "had a fab time",
         "author": "sharon",
-
+        "stars": 5
     }
 
   ];
