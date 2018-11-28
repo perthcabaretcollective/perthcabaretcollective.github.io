ad1a2e271ea71146e472470f2e2835811c1ca03d
Jess <harry.brooker999@gmail.com>
2018-11-28 22:11:56 +0800

    photos

--- js/read/readGallery.js
+++ js/read/readGallery.js
@@ -1,10 +1,10 @@
 var galleryFeed = document.getElementById("galleryFeed");
-var galleryString = "<div class=\"container\">";
+var galleryString = "<div class=\"row\">";
 var galleryCount;
 
 for(galleryCount = 0; galleryCount< galleryID.length; galleryCount++){
-    galleryString = galleryString + "<div class=\"col-xs-4\"\
-    <img src=\" images\/gallery\/" + galleryID[galleryCount] + ".jpg\"><\/div>";
+    galleryString = galleryString + "<div class=\"gallery-product col-lg-4 col-md-4 col-sm-4 col-xs-6\"\
+    <img src=\"images\/gallery\/" + galleryID[galleryCount] + "\" class=\"img-responsive\"><\/div>";
 }
 galleryString = galleryString + "<\/div>";
 galleryFeed.innerHTML = galleryString;
