78ae4e46d14f3a6c0851498611f94147b4b32ff6
Jess <harry.brooker999@gmail.com>
2018-11-28 20:58:36 +0800

    ADD REVIEW

--- js/reviews.js
+++ js/reviews.js
@@ -8,6 +8,11 @@ var reviewData = [
         "description": "had a fab time",
         "author": "sharon",
         "stars": 5
+    },
+    {
+        "description": "A time capsule of wonder",
+        "author": "FRINGE FEED",
+        "stars": 5
     }
 
   ];
