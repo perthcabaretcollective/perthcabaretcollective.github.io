50cc3b110b64a6eef1781e3ea51a5da858da6ad9
Jess <harry.brooker999@gmail.com>
2018-11-28 22:16:01 +0800

    fix photos

--- js/read/readGallery.js
+++ js/read/readGallery.js
@@ -3,8 +3,8 @@ var galleryString = "<div class=\"row\">";
 var galleryCount;
 
 for(galleryCount = 0; galleryCount< galleryID.length; galleryCount++){
-    galleryString = galleryString + "<div class=\"gallery-product col-lg-4 col-md-4 col-sm-4 col-xs-6\"\
-    <img src=\"images\/gallery\/" + galleryID[galleryCount] + "\" class=\"img-responsive\"><\/div>";
+    galleryString = galleryString + "<div class=\"gallery-product col-lg-4 col-md-4 col-sm-4 col-xs-6\">\
+    <img src=\"images\/gallery\/" + galleryID[galleryCount] + "\" class=\"img-responsive\" alt=\"piccy\"><\/div>";
 }
 galleryString = galleryString + "<\/div>";
 galleryFeed.innerHTML = galleryString;
