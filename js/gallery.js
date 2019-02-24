8eafb89f8be6bcc0a247ab8c94c7b06c54d74682
Jess <harry.brooker999@gmail.com>
2019-02-24 20:39:47 +0800

    fix media

--- js/gallery.js
+++ js/gallery.js
@@ -1,19 +1,20 @@
 var galleryID = [
     //612x408
-'19.jnp',
-'20.jnp',
-'21.jnp',
+'21.jpg',
     '18.jpg',
     '1.jpeg',
     '15.PNG',
     '3.jpeg',
     '6.jpeg',
+    '20.jpg',
     '10.jpeg',
     '16.PNG',
     '13.png',
     '14.png',
     '11.jpeg',
     '5.jpeg',
+    '19.jpg',
+
     '17.PNG',
 
 
